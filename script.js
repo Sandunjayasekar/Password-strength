b61f64bdac7209d4f1da5f92f65f723af2b2b472
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-18 17:21:49 +0530

    strong message function added

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ var msg = document.getElementById('text');
 var str = document.getElementById('strength');
 
 password.addEventListener('input', () => {
-    if (password.value.length < 8) {
-        msg.innerHTML = 'Password must be at least 8 characters';
-        str.style.color = '#ff0000';
+    if (password.value.length > 0) {
+        msg.style.display = 'block';
     } else {
-        msg.innerHTML = 'Password Strength: <span id="strength"></span>';
-        str.style.color = '#fff';
-        checkStrength(password.value);
+        msg.style.display = 'none';
     }
 });
